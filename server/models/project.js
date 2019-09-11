efcd7011fd7cadcb28f48594e7ac4bbc2b32317f
Asad Siddiqui <asadsid95@gmail.com>
2019-09-11 11:36:41 -0400

    Added description key in project schema

--- server/models/project.js
+++ server/models/project.js
@@ -19,6 +19,11 @@ const projectSchema = new mongoose.Schema(
       type: String,
       required: true
     },
+    description: {
+      type: String,
+      required: true,
+      maxlength: 500
+    },
     photos: [
       {
         photo: {
